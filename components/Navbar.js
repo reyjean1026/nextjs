bcc1c4e6cad6136c4ff6dea431d9864e881bd0c7
sadhiq <sadhiqahamed650@gmail.com>
2022-07-28 17:01:32 +0530

    navbar added
    navbar added

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white pl-6 py-6 justify-between relative'>
+      
+    </div>
+  )
+}
+
+export default Navbar
