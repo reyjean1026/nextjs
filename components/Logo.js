1427aa84be73c2b8a25ad51805179b0fd18a35a3
sadhiq <sadhiqahamed650@gmail.com>
2022-07-28 17:08:15 +0530

    logo added
    logo added

---
+++ components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+      <span className='text-black text-3xl font-medium mb-6'>LOGO</span>
+    </div>
+  )
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <div className='bg-white pl-6 py-6 justify-between relative shadow-md'>
-      
+      <Logo/>
     </div>
   )
 }
