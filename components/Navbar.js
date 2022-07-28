62298d93f2bfa408d094f499b61f853d77d62662
sadhiq <sadhiqahamed650@gmail.com>
2022-07-28 17:17:46 +0530

    menu icon added
    menu icon added

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Logo from './Logo'
 
 const Navbar = () => {
-  return (
-    <div className='bg-white pl-6 py-6 justify-between relative shadow-md'>
-      <Logo/>
-    </div>
-  )
+    return (
+        <div className='bg-white pl-6 py-6 justify-between relative shadow-md'>
+            <Logo />
+            <div className='text-4xl text-black absolute top-6 right-6 cursor-pointer'><ion-icon name="menu-outline"></ion-icon>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
