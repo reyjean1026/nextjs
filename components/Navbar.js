de33c0a4326553be58e613eee6fbe9bc0c7a2f85
sadhiq <sadhiqahamed650@gmail.com>
2022-07-28 17:41:02 +0530

    menu open close function added
    menu open close function added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import Logo from './Logo'
 import NavLinks from './NavLinks'
 
 const Navbar = () => {
+    let [isOpen, setIsOpen] = useState(false);
     return (
         <div className='bg-white pl-6 py-6 justify-between relative shadow-md'>
             <Logo />
-            <div className='text-4xl text-black absolute top-6 right-6 cursor-pointer'><ion-icon name="menu-outline"></ion-icon>
+            <div onClick={() => setIsOpen(!isOpen)} className='text-4xl text-black absolute top-6 right-6 cursor-pointer'><ion-icon name={isOpen ? 'close' : 'menu'}></ion-icon>
             </div>
-            <ul>
-                <NavLinks/>
-                <Button/>
+            <ul className={`bg-white pb-12 z-[2] w-full transition-all duration-300 ease-in left-0 absolute pl-7 ${isOpen ? 'top-20 opacity-100' : 'opacity-0 -top-96'}`}>
+                <NavLinks />
+                <Button />
             </ul>
         </div>
     )
