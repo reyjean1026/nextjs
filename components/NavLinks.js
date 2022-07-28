ca1756a44a24f9b4985bfdbeb155043e7a9b5c27
sadhiq <sadhiqahamed650@gmail.com>
2022-07-28 17:56:47 +0530

    customized for medium screens
    customized for medium screens

--- components/NavLinks.js
+++ components/NavLinks.js
@@ -10,14 +10,14 @@ export default function NavLinks() {
         { 'name': 'Pricing', 'path': '/pricing' },]
     // Getting links and looping them through li
     const listItems = links.map((link) =>
-            <li key={link.name} className='text-black text-lg font-medium hover:text-cyan-400 duration-300 mt-4 cursor-pointer border p-3'>
+            <li key={link.name} className='text-black text-lg font-medium hover:text-cyan-400 duration-300 mt-4 cursor-pointer border p-3 md:ml-8 md:mb-0 mb-6 md:mt-0 md:p-0 md:border-none'>
                 <Link href={link.path}>
                 <a>{link.name}</a>
             </Link>
            </li>
     );
     return (
-        <div className=''>
+        <div className='md:flex'>
             {listItems}
         </div>
     )
--- components/Navbar.js
+++ components/Navbar.js
@@ -6,11 +6,11 @@ import NavLinks from './NavLinks'
 const Navbar = () => {
     let [isOpen, setIsOpen] = useState(false);
     return (
-        <div className='bg-white pl-6 py-6 justify-between relative shadow-md'>
+        <div className='bg-white pl-6 py-6 justify-between relative shadow-md md:flex'>
             <Logo />
-            <div onClick={() => setIsOpen(!isOpen)} className='text-4xl text-black absolute top-6 right-6 cursor-pointer'><ion-icon name={isOpen ? 'close' : 'menu'}></ion-icon>
+            <div onClick={() => setIsOpen(!isOpen)} className='text-4xl text-black absolute top-6 right-6 cursor-pointer md:hidden '><ion-icon name={isOpen ? 'close' : 'menu'}></ion-icon>
             </div>
-            <ul className={`bg-white pb-12 z-[2] w-full transition-all duration-300 ease-in left-0 absolute pl-7 ${isOpen ? 'top-20 opacity-100' : 'opacity-0 -top-96'}`}>
+            <ul className={`bg-white pb-8 z-[2] w-full transition-all duration-300 ease-in left-0 absolute pl-7 md:flex md:j md:items-center md:mr-6 md:pb-0 md:z-auto md:w-auto md:static ${isOpen ? 'top-20 opacity-100' : 'opacity-0 -top-96 md:opacity-100'}`}>
                 <NavLinks />
                 <Button />
             </ul>
